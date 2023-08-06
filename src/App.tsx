7c55ed0c63e767ace250db1c1d4ee2d45c2615fb
hyeonjin25 <shj41738866@gmail.com>
2023-08-06 12:35:46 +0900

    feat: global style 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Fonts } from './utils/GlobalFonts';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Fonts.num1>하잇</Fonts.num1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/utils/GlobalStyles.tsx
@@ -0,0 +1,25 @@
+// colors
+export const black = '#000000';
+export const white = '#FFFFFF';
+
+export const primary = '#4397AA';
+
+export const gray01 = '#666666';
+export const gray02 = '#999999';
+export const gray03 = '#BFBFBF';
+export const gray04 = '#CBCBCB';
+export const gray05 = '#D9D9D9';
+export const gray06 = '#F0F0F0';
+export const gray07 = '#F1F4F6';
+export const gray08 = '#F9FBFD';
+
+export const colors = {
+  black,
+  white,
+  primary,
+  gray08,
+  gray06,
+  gray03,
+  gray05,
+  gray01,
+};
