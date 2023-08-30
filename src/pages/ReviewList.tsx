f1862435c6d24c39aa5f2897a73a788db6f3548d
hyeonjin25 <shj41738866@gmail.com>
2023-08-30 20:14:23 +0900

    feat: 부모로 height 상태 전달하는 기능 추가 (related to: #25) 처음 렌더링 시 전달하는 height가 부모 컴포넌트에서 iframe이 load 되기 전에 전달되어 부모에서 전달 받지 못하는 현상이 발생 -> iframe이 로드되어 부모로 부터 메세지를 전송 받은 후에 height를 보내는 방식으로 해결

--- src/pages/ReviewList.tsx
+++ src/pages/ReviewList.tsx
@@ -2,10 +2,14 @@ import KeywordStats from '@/components/KeywordStats';
 import ReviewSortingList from '@/components/ReviewSortingList';
 import ReviewStats from '@/components/ReviewStats';
 import { Margin } from '@/ui/margin/margin';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { styled } from 'styled-components';
 
 export default function ReviewList() {
+  const componentRef = React.useRef<HTMLDivElement>(null);
+
+  const [heightChange, setHeightChange] = React.useState(false);
+
   const [rating, setRating] = React.useState(5.0);
 
   // 5,4,3,2,1점
@@ -54,11 +58,35 @@ export default function ReviewList() {
     },
   ]);
 
+  useEffect(() => {
+    const handleMessage = (e: MessageEvent) => {
+      if (!e.data.type) return;
+      if (e.data.type === 'loaded') sendHeightToParent();
+      console.log('부모로 부터 온 메세지:', e.data);
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
+  const sendHeightToParent = () => {
+    if (!componentRef.current) return;
+    const message = componentRef.current?.offsetHeight + 30;
+    window.parent.postMessage({ type: 'height', message: message }, '*');
+  };
+
+  useEffect(() => {
+    sendHeightToParent();
+  }, [heightChange]);
+
   return (
-    <Container>
+    <Container ref={componentRef}>
       <ReviewStats rating={rating} scoreList={scoreList} />
       <Margin margin={'30px 0 0 0'} />
-      <KeywordStats />
+      <KeywordStats setHeightChange={setHeightChange} />
       <Margin margin={'30px 0 0 0'} />
       <ReviewSortingList reviewList={reviewList} />
     </Container>
